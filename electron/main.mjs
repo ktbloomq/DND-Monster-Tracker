f73dae66d8cd3fb9cf8be08647f1a1cb7b5afb4e
Kevin Bloomquist <ktb@bloomquist.com>
2024-08-02 13:26:51 -0700

    electron app

---
+++ electron/main.mjs
@@ -0,0 +1,43 @@
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import serve from 'electron-serve';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const appServe = app.isPackaged ? serve({
+  directory: path.join(__dirname, "../out")
+}) : null;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+    }
+  });
+
+  if (app.isPackaged) {
+    appServe(win).then(() => {
+      win.loadURL("app://-");
+    });
+  } else {
+    win.loadURL("http://localhost:3000");
+    win.webContents.openDevTools();
+    win.webContents.on("did-fail-load", (e, code, desc) => {
+      win.webContents.reloadIgnoringCache();
+    });
+  }
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin"){
+        app.quit();
+    }
+});
